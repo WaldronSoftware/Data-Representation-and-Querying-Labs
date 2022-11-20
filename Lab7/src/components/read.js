4bd26cdf38b6189705a786a82ca206fe6626febc
Ronan Waldron <ronanwaldronsoftware@gmail.com>
2022-11-20 17:36:06 +0000

    Lab7

---
+++ Lab7/src/components/read.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Books } from './books';
+import axios from "axios";
+
+
+export class Read extends React.Component{
+    state = {
+        books: [
+            
+        ]
+    }
+
+    componentDidMount()
+    {
+        // Request for async
+        axios.get("http://localhost:4000/api/books/").then(
+            (resp =>
+            {
+                // State update
+                console.log(resp.data);
+                this.setState({ books: resp.data });
+            })
+        ).catch((err) =>
+        {
+            console.log(err)
+        })
+    }
+
+
+    render()
+    {
+        return (
+            <div >
+                <Books books={this.state.books}></Books>
+            </div>
+        );
+    }
+}
+
